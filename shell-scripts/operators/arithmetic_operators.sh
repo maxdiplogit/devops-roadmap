f606e3399a9e7f34c9071b1c4002723ebe0adce0
maxdiplogit <maxdiplogit@gmail.com>
2025-10-01 12:11:47 +0530

    study basic operators, example of file operators remains

---
+++ shell-scripts/operators/arithmetic_operators.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+a=10
+b=20
+
+val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % a : $val"
+
+val=`expr $a == $b`
+echo "a == b : $val"
